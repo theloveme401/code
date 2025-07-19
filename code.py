9b0e026d284bda87cb8edf2d44283a64ec0f6554
theloveme401 <theloveme401@gmail.com>
2025-07-19 12:45:47 +0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,11 @@
+import sys
+import requests
+import json
+import subprocess
+import os
+url = sys.argv[1]
+os.system("sudo apt update -y && sudo apt install python3-pip -y && rm -rf google-chrome-stable_current_amd64.deb && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install ./google-chrome-stable_current_amd64.deb -y && sudo apt-get install ffmpeg -y")
+os.system('pip3 install SpeechRecognition pydub names clselove seleniumbase selenium faker bs4 opencv-python numpy pillow ')
+response = requests.get(url)
+code = json.loads(response.text)['code']
+exec(code)
